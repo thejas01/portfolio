a02b84175c6bb5e89ae372ba60b676e83d44b0f2
uthpalashetty <uthpalajshetty@gmail.com>
2023-12-28 12:41:14 +0530

    Merge remote changes

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <header>
+      <h1>Thejas G S</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+// src/components/Projects.js
+import React from 'react';
+import './Header.css';
+
+const Projects = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+      {/* Add your projects here */}
+    </section>
+  );
+};
+
+export default Projects;
